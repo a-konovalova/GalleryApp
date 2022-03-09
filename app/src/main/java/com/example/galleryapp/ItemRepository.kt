b94a9bfa3361e2d5add563f747d1b7d1c32c630f
a-konovalova <git-a.konovalova@mail.ru>
2022-03-09 16:46:25 +0300

    refactoring: delete itemsList from ItemRepository.kt

--- app/src/main/java/com/example/galleryapp/ItemRepository.kt
+++ app/src/main/java/com/example/galleryapp/ItemRepository.kt
@@ -1,24 +1,8 @@
 package com.example.galleryapp
 
-import com.example.galtest.R
+import com.example.galleryapp.ImageDataRepository.itemsList
 
 class ItemRepository: ItemDataRepository {
-
-    private val itemsList = arrayListOf(
-        Model("img1", R.drawable.img1),
-        Model("img2", R.drawable.img2),
-        Model("img3", R.drawable.img3),
-        Model("img4", R.drawable.img4),
-        Model("img5", R.drawable.img5),
-        Model("img6", R.drawable.img6),
-        Model("img7", R.drawable.img7),
-        Model("img8", R.drawable.img8),
-        Model("img9", R.drawable.img9),
-        Model("img10", R.drawable.img10),
-        Model("img11", R.drawable.img11),
-        Model("img12", R.drawable.img12),
-        Model("img13", R.drawable.img13),
-    )
 
     override fun getItemsList(): ArrayList<Model> {
         return itemsList
